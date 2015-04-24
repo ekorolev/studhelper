892fdf1e0261b880cd9919c3e4d26faab182deca
ekorolev <ea.korolev@mail.ru>
2015-04-25 01:54:32 +0300

    svg

---
+++ client/js/map.js
@@ -0,0 +1,5 @@
+$(function () {
+	console.log("Document has loaded.");
+
+	var svgdom = document.getElementById('svgmap');
+});
--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
 var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.use(express.static(path.normalize(__dirname+'/../client/')));
+
+app.get('/', function (req, res) {
+	res.sendFile(path.normalize(__dirname+'/../client/index.html'));
+});
+
+app.listen(8171, function () {
+	console.log("Server has started.");
+});
